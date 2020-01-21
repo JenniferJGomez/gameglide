81fdf9dec9c1c7832a85fff944a9e4d5f1daeb55
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-01-21 14:06:04 -0500

    some more

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,45 @@
 class GamesController < ApplicationController
+    before_action :get_game, only: [:show, :edit, :update, :destroy]
+
+def index 
+  @games = Game.all 
+end 
+
+def new 
+    @game = Game.new 
+end 
+
+def create 
+    @game = Game.create(game_params)
+    if 
+    @game.save 
+    redirect_to game_path(@game)
+        
+    else
+        render :new
+    end
+end 
+
+def edit 
+end 
+
+def update 
+    @game.update(game_params)
+    redirect_to game_path(@game)
+end 
+
+def destroy 
+    @game.destroy
+    redirect_to games_path
+end 
+
+private 
+   
+def get_game
+    @game = Game.find(params[:id])
+end 
+
+def game_params 
+    params.require(:game).permit(:title, :multiplayer, :online, :platform, :genre_id)
+end 
 end
