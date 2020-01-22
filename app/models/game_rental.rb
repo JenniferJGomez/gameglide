ac397aa3bf3e183ce0a1c616625fa7a49d119f3e
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-01-22 15:35:20 -0500

    ok

--- app/models/game_rental.rb
+++ app/models/game_rental.rb
@@ -2,6 +2,9 @@ class GameRental < ApplicationRecord
     belongs_to :user 
     belongs_to :game 
     belongs_to :review, required: false
+    validates :user_id, presence: true 
+    validates :game_id, presence: true 
+    
 
     # validates :review_id, :numericality => true, :allow_nil => true
 end
