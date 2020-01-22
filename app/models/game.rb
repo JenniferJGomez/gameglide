114ff6ac8fdca30e7f250e33ccd50c8f6d7c05b6
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-01-22 16:35:58 -0500

    i hope

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,10 @@ class Game < ApplicationRecord
     has_many :game_rentals 
     has_many :users, through: :game_rentals
     belongs_to :genre 
+    validates :title, presence: true 
+    validates :multiplayer, presence: true 
+    validates :online, presence: true 
+    validates :platform, presence: true 
+    
     
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
     belongs_to :game_rental 
+    validates :content, presence: true 
+    validates :content, presence: true 
 end
