29253838382a38f417c47c564f2c8cadffcc39d1
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-01-22 16:17:11 -0500

    Created helper methods in Game Model to build associations to reviews

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,27 @@
 class Game < ApplicationRecord
+
     has_many :game_rentals 
     has_many :users, through: :game_rentals
     belongs_to :genre 
     
+    def reviews_content
+        review = self.game_rentals.map do |rental|
+            rental.review
+        end
+            review.map do |rev|
+                rev.content
+            end
+    end
+
+    def reviews_rating
+        review = self.game_rentals.map do |rental|
+            rental.review
+        end
+            review.map do |rev|
+                rev.rating
+            end
+    end
+
+
 end
+
