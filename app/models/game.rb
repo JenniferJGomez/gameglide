08bc8297749658c66c85e1e63b2fc94c5cabdbf1
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-01-22 16:55:47 -0500

    updated game model association and show form

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,7 @@ class Game < ApplicationRecord
 
     has_many :game_rentals 
     has_many :users, through: :game_rentals
+    has_many :reviews, through: :game_rentals
     belongs_to :genre 
     validates :title, presence: true 
     validates :multiplayer, presence: true 
@@ -9,23 +10,23 @@ class Game < ApplicationRecord
     validates :platform, presence: true 
     
     
-    def reviews_content
-        review = self.game_rentals.map do |rental|
-            rental.review
-        end
-            review.map do |rev|
-                rev.content
-            end
-    end
+    # # def reviews_content
+    # #     review = self.game_rentals.map do |rental|
+    # #         rental.review
+    # #     end
+    # #         review.map do |rev|
+    # #             rev.content
+    # #         end
+    # # end
 
-    def reviews_rating
-        review = self.game_rentals.map do |rental|
-            rental.review
-        end
-            review.map do |rev|
-                rev.rating
-            end
-    end
+    # # def reviews_rating
+    # #     review = self.game_rentals.map do |rental|
+    # #         rental.review
+    # #     end
+    # #         review.map do |rev|
+    # #             rev.rating
+    # #         end
+    # end
 
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
     belongs_to :game_rental 
     validates :content, presence: true 
-    validates :content, presence: true 
+    validates :rating, presence: true 
 end
