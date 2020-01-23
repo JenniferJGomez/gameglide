2a12b86536c9ca720a2cf25da7b4a578e9876630
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-01-23 09:19:21 -0500

    Pushed

--- app/controllers/concerns/sessions_controller.rb
+++ app/controllers/concerns/sessions_controller.rb
@@ -4,9 +4,13 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(username: params[:user][:username])
-        if user && user.authenticate(params[:user][:pasword])
-            session[:user_id]= user.id
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:pasword])
+            session[:user_id]= @user.id
+            redirect_to games_path
+        else
+        flash.notice = "No user found with that name"
+        render :new
         end
     end
 
