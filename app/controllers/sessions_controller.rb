6afd9c75a3974cc7cc56903695b28470a02314f4
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-01-23 11:06:11 -0500

    controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new
+
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id]= @user.id
+            redirect_to games_path
+        else
+        flash.notice = "No user found with that name"
+        render :new
+        end
+    end
+
+    def destroy
+        session.clear
+        redirect_to '/login'
+
+    end
+end
